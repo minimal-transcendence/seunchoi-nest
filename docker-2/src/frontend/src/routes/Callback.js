3f9b2b691788a1ee18eb870bd39d200c034e8e1a
Seungjoon Choi <tmdwns0845@naver.com>
2023-07-31 05:11:55 +0900

    Add(react): get jwt on /callback

--- docker-2/src/frontend/src/routes/Callback.js
+++ docker-2/src/frontend/src/routes/Callback.js
@@ -1,7 +1,36 @@
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function Callback() {
+    const [login, setLogin] = useState();
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get('code');
+
+    const getLogin = async (code) => {
+        const json = await (
+            await fetch(
+                'http://localhost/api/auth/login?code=' + code
+                )
+        ).json();
+        console.log(json.message);
+        setLogin(json.message);
+    }
+
+    useEffect(() => {
+        getLogin(code);
+    },[]);
+
+    // redirect
+    if (code) {
+        return (
+            <div>
+                login message: {login};
+            </div>
+        );
+    }
     return (
         <div>
-            <h1>CALLBACK PAGE</h1>
+            <h1>No code</h1>
         </div>
     );
 }
