7a5246ba7de755f9aff00164ff3b1cba91d7ad52
SeungJoonChoi <tmdwns0845@naver.com>
2023-08-14 17:25:29 +0900

    Add(pong): pong game - nextjs

---
+++ pong/app/page.tsx
@@ -0,0 +1,229 @@
+'use client'
+
+import { useRef, useState, useEffect, MutableRefObject } from "react"
+
+export default function Pong() {
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // const contextRef = useRef(null);
+  // const [canvasTag, setCanvasTag] = useState([]);
+
+
+
+  useEffect(() => {
+    // Initialize Canvas
+    const canvas: any = canvasRef.current;
+    canvas.width = window.innerWidth * 0.5;
+    canvas.height = window.innerHeight;
+    const context = canvas.getContext("2d");
+
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
+
+    // Paddle
+    let paddleHeight = 10;
+    let paddleWidth = 50;
+    let paddleDiff = 25;
+    let paddleX = [canvas.width / 2 - paddleWidth / 2,
+      canvas.width / 2 - paddleWidth / 2];
+    let trajectoryX = [0, 0];
+    let playerMoved = true;
+
+    // Ball
+    let ballX = canvas.width / 2;
+    let ballY = canvas.height / 2;
+    let ballRadius = 5;
+    let ballDirection = 1;
+
+    // Speed
+    let speedY = 5;
+    let speedX = 0.07;
+
+    /*-----------------------------------------------------*/
+
+    // Coordinate of Ball
+    // Adjust Ball Movement
+    const ballMove = () => {
+      // Vertical Speed
+      ballY += speedY * ballDirection;
+      // Horizontal Speed
+      if (playerMoved) {
+        ballX += speedX;
+      }
+
+      // 소켓 이벤트
+      // socket.emit("ballMove", {
+      //   ballX,
+      //   ballY,
+      //   score,
+      // });
+    }
+
+    const ballReset = () => {
+      ballX = canvas.width / 2;
+      ballY = canvas.height / 2;
+      speedY = 3;
+      // todo -console.log("in ballreset ", refreeRoom);
+      // socket.emit("ballMove", {
+      //   ballX,
+      //   ballY,
+      //   score,
+      // });
+    }
+
+    const ballBoundaries = () => {
+      // Bounce off Left Wall
+      if (ballX < 0 && speedX < 0) {
+        speedX = -speedX;
+      }
+      // Bounce off Right Wall
+      if (ballX > canvas.width && speedX > 0) {
+        speedX = -speedX;
+      }
+      // Bounce off player paddle (bottom)
+      if (ballY > canvas.height - paddleDiff) {
+        if (ballX >= paddleX[0] && ballX <= paddleX[0] + paddleWidth) {
+          // Add Speed on Hit
+          if (playerMoved) {
+            speedY += 1;
+            // Max Speed
+            if (speedY > 10) {
+              speedY = 10;
+            }
+          }
+          ballDirection = -ballDirection;
+          trajectoryX[0] = ballX - (paddleX[0] + paddleDiff);
+          speedX = trajectoryX[0] * 0.3;
+        } else {
+          // todo - Reset Ball, add to Computer Score
+          ballReset();
+          // score[1]++;
+        }
+      }
+      // Bounce off computer paddle (top)
+      if (ballY < paddleDiff) {
+        if (ballX >= paddleX[1] && ballX <= paddleX[1] + paddleWidth) {
+          // Add Speed on Hit
+          if (playerMoved) {
+            speedY += 1;
+            // Max Speed
+            if (speedY > 10) {
+              speedY = 10;
+            }
+          }
+          ballDirection = -ballDirection;
+          trajectoryX[1] = ballX - (paddleX[1] + paddleDiff);
+          speedX = trajectoryX[1] * 0.3;
+        } else {
+          // Reset Ball, Increase Computer Difficulty, add to Player Score
+          // todo
+          ballReset();
+          // score[0]++;
+        }
+      }
+    }
+
+    /*-----------------------------------------------------*/
+
+    // Draw
+    // Canvas Background
+    const drawBackground = () => {
+      context.fillStyle = "black";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    // Paddle
+    const drawPaddle = () => {
+      context.fillStyle = "white";
+      // Bottom Paddle
+      context.fillRect(paddleX[0], canvas.height - 20, paddleWidth, paddleHeight);
+      // Top Paddle
+      context.fillRect(paddleX[1], 10, paddleWidth, paddleHeight);
+    }
+
+    // Ball
+    const drawBall = () => {
+      context.beginPath();
+      context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
+      context.fillStyle = "white";
+      context.fill();
+    }
+
+    // Dashed Center Line
+    const drawCenterLine = () => {
+      context.beginPath();
+      context.setLineDash([4]);
+      context.moveTo(0, canvas.height / 2);
+      context.lineTo(canvas.width, canvas.height / 2);
+      context.strokeStyle = "grey";
+      context.stroke();
+    }
+
+    // Draw All Context
+    const draw = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      drawBackground();
+      drawCenterLine();
+      drawPaddle();
+      ballMove();
+      ballBoundaries();
+      drawBall();
+    }
+
+    /*-----------------------------------------------------*/
+
+    
+
+    // setCanvasTag(canvas);
+    // setInterval(() => {
+    //   context.clearRect(0, 0, canvas.width, canvas.height);
+    //   // Canvas Background
+    //   context.fillStyle = "black";
+    //   context.fillRect(0, 0, canvas.width, canvas.height);
+
+    //   // Paddle Color
+    //   context.fillStyle = "white";
+
+    //   // Bottom Paddle
+    //   context.fillRect(paddleX[0], canvas.height - 20, paddleWidth, paddleHeight);
+
+    //   // Top Paddle
+    //   context.fillRect(paddleX[1], 10, paddleWidth, paddleHeight);
+
+    //   // Dashed Center Line
+    //   context.beginPath();
+    //   context.setLineDash([4]);
+    //   context.moveTo(0, canvas.height / 2);
+    //   context.lineTo(canvas.width, canvas.height / 2);
+    //   context.strokeStyle = "grey";
+    //   context.stroke();
+
+    //   // Ball
+    //   context.beginPath();
+    //   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
+    //   context.fillStyle = "white";
+    //   context.fill();
+
+    //   // context.beginPath();
+    //   // context.arc(x, y, 20, 0, Math.PI * 2);
+    //   // context.fillStyle = "green";
+    //   // context.fill();
+    //   // context.closePath();
+
+    //   x += speedX;
+    //   y += speedY;
+    // }, 60); // 인터벌 속도 조정 - 프레임?
+    setInterval(draw, 10);
+  }, [canvasRef])
+
+  // console.log("canvasTag :", canvasTag);
+
+  return (
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <div>
+        <canvas ref={canvasRef}/>
+      </div>
+    </main>
+  )
+}
